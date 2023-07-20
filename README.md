07669009be1cd046a254d0a2d041818b3863d3dc
gardenboi <dawidgrden.99@gmail.com>
2023-07-20 16:48:48 +0200

    react go postgis template v1

---
+++ README.md
@@ -0,0 +1,6 @@
+
+THIS IS A TEMPLATE
+
+resources !
+
+https://www.xloypaypa.pub/react-typescript-leaflet-openstreetmap-integration/
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import OSM from "./components/map/OpenStreetMap";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <OSM></OSM>
+    </>
+  )
+}
+
+export default App
