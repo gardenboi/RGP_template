0cccad8d6a3c3b41d9d0affe8ad505edddade6f3
gardenboi <dawidgrden.99@gmail.com>
2023-08-04 16:09:01 +0200

    new backend template wip

---
+++ backend/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/gardenboi/kmzerobackend/graph" // Replace username with your github username
+	"github.com/gin-gonic/gin"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
+)
+
+const connStr = "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	db, err := sql.Open("postgres", connStr)
+	// Setting up Gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
+}
